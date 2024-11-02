d5a515d1bd2ba536c1801e6799b5960ff5c9161d
Bryan Jiménez Guerrero <96020118+bryanbjg@users.noreply.github.com>
2024-11-02 10:55:00 -0400

    feat: update packeage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 };
