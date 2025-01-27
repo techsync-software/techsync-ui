99f4ca26fd10a3b1e4cc0c8d9dce734a0d47d42c
Bryan Jiménez Guerrero <96020118+bryanbjg@users.noreply.github.com>
2025-01-26 21:54:41 -0400

    feat: update footer

--- src/app/home/components/footer/footer.component.ts
+++ src/app/home/components/footer/footer.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  public currentYear: number = new Date().getFullYear();
 }
